30f1055e40a8a3c1ec292c8ec432934bbce75153
Karina <pioni.mika@mail.ru>
2021-11-26 10:50:53 +0300

    updated 1.0

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -4,15 +4,16 @@ import org.testng.annotations.Test;
 
 public class SauceDemoTest extends BaseTest {
 
-        String userNameById = "user-name";
-        String passwordByXpath = "//*[@id='password']";
-        String loginButton = "login-button";
-        String itemByContainsText = "//div[contains(text(), 'Sauce Labs Backpack')]";
-        String itemPrice = "//div[div[a[@id='item_4_title_link']]]/div[@class='pricebar']/div";
-        String addToCartButton = "//*[@id='add-to-cart-sauce-labs-backpack']";
-        String shoppingCartLogo = ".shopping_cart_link";
-        String itemInTheShoppingCart = "//*[@id='item_4_title_link']/div";
-        String itemPriceInTheShoppingCart = ".item_pricebar";
+    String userNameById = "user-name";
+    String passwordByXpath = "//*[@id='password']";
+    String loginButton = "login-button";
+    String itemByContainsText = "//div[contains(text(), 'Sauce Labs Backpack')]";
+    String itemPrice = "//div[div[a[@id='item_4_title_link']]]/div[@class='pricebar']/div";
+    String addToCartButton = "//*[@id='add-to-cart-sauce-labs-backpack']";
+    String shoppingCartLogo = ".shopping_cart_link";
+    String itemInTheShoppingCart = "//*[@id='item_4_title_link']/div";
+    String itemPriceInTheShoppingCart = ".inventory_item_price";
+
 
     @Test
     public void sauceDemo() {
@@ -22,16 +23,19 @@ public class SauceDemoTest extends BaseTest {
         driver.findElement(By.xpath(passwordByXpath)).sendKeys("secret_sauce");
         driver.findElement(By.name(loginButton)).click();
         driver.findElement(By.xpath(itemByContainsText)).getText();
-        driver.findElement(By.xpath(itemPrice)).getText();
+        String itemPriceValue = driver.findElement(By.xpath(itemPrice)).getText();
         itemPrice = "$\n$29.99";
         driver.findElement(By.xpath(addToCartButton)).click();
         driver.findElement(By.cssSelector(shoppingCartLogo)).click();
+
         boolean isDisplayed = driver.findElement(By.xpath("//span[contains(text(), 'Your Cart')]")).isDisplayed();
         Assert.assertTrue(isDisplayed, "Не перешли на страницу корзины");
+
         driver.findElement(By.xpath(itemInTheShoppingCart)).getText();
-        driver.findElement(By.cssSelector(itemPriceInTheShoppingCart)).getText();
+        String itemPriceValueInTheShoppingCart = driver.findElement(By.cssSelector(itemPriceInTheShoppingCart)).getText();
         itemPriceInTheShoppingCart = "$\n$29.99";
-        Assert.assertEquals(itemPrice, itemPriceInTheShoppingCart);
+
+        Assert.assertEquals(itemPriceValue, itemPriceValueInTheShoppingCart);
 
 
     }
